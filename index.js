e79614ab7df01a6879e5cbe2256f28f0f7185eda
Bhargav Gembali <113098503+BhargavGembali@users.noreply.github.com>
2025-07-14 09:01:41 +0000

    commit

--- index.js
+++ index.js
@@ -6,12 +6,16 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT ;
+const PORT = process.env.PORT || 5001;
 
 app.use(cors());
 
 app.get('/search', async (req, res) => {
   const { query } = req.query;
+  if (!query) {
+    res.status(400).json({ error: 'Missing query parameter' });
+    return;
+  }
   try {
     const results = await fetchBookLinks(query);
     res.json(results);
@@ -22,20 +26,15 @@ app.get('/search', async (req, res) => {
 });
 
 const fetchBookLinks = async (query) => {
-  const url = `https://archive.org/advancedsearch.php?q=${query}+mediatype:texts&fl[]=title&fl[]=creator&fl[]=year&fl[]=identifier&output=json&rows=20`;
-  try {
-    const response = await axios.get(url);
-    return response.data.response.docs.map(book => ({
-      title: book.title,
-      author: book.creator ? book.creator : 'Unknown',
-      publishYear: book.year,
-      link: `https://archive.org/details/${book.identifier}`,
-      cover: `https://archive.org/services/img/${book.identifier}`
-    }));
-  } catch (error) {
-    console.error('Error fetching data from Archive.org:', error);
-    throw error;
-  }
+  const url = `https://archive.org/advancedsearch.php?q=${encodeURIComponent(query)}+mediatype:texts&fl[]=title&fl[]=creator&fl[]=year&fl[]=identifier&output=json&rows=20`;
+  const response = await axios.get(url);
+  return response.data.response.docs.map(book => ({
+    title: book.title,
+    author: book.creator ? book.creator : 'Unknown',
+    publishYear: book.year,
+    link: `https://archive.org/details/${book.identifier}`,
+    cover: `https://archive.org/services/img/${book.identifier}`
+  }));
 };
 
 app.listen(PORT, () => {
